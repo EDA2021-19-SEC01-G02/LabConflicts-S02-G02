9f45b0e881b34848bc0c76e55d022be81369143c
Alejandro Palomino <a.palomino@uniandes.edu.co>
2021-06-17 18:16:53 -0500

    cambios finales

--- App/view.py
+++ App/view.py
@@ -50,7 +50,7 @@ def loadBooks():
     """
     Carga los libros
     """
-    return controller.loadBooks('GoodReads/books-small.csv')
+    return controller.loadBooks('GoodReads/books.csv')
 
 
 def loadTags():
@@ -63,7 +63,7 @@ def loadBooksTags():
     """
     Cargar los Tags de libros
     """
-    return controller.loadBooks('GoodReads/book_tags-small.csv')
+    return controller.loadBooks('GoodReads/book_tags.csv')
 
 
 """
